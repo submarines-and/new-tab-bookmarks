87fbd40b90c258ac27d6ca8b2595e9a61cee5419
Johannes Sörensen <johannes.sorensen@ingka.ikea.com>
2022-05-26 18:35:36 +0200

    Show favicons for folders in a grid

--- src/main.tsx
+++ src/main.tsx
@@ -44,11 +44,21 @@ class Bookmarks extends React.Component<any, State> {
 
             <div className='d-flex align-items-center border rounded mb-2' style={{ height: 100, width: 100 }} onClick={(event) => this.openFolder(event, bookmark)}>
               {bookmark.url && <img className='mx-auto' height='24' width='24' src={`http://www.google.com/s2/favicons?domain=${bookmark.url}`} />}
+              {!bookmark.url && this.renderFolderIcons(bookmark)}
             </div>
 
             <small className='d-block text-dark text-center text-wrap' style={{ width: 100 }}>{bookmark.title}</small>
           </a>
         ))}
+      </div>
+    );
+  }
+
+  /** For folders, show contained items in a grid  */
+  private renderFolderIcons(folder: Bookmark) {
+    return (
+      <div className='d-flex flex-wrap'>
+        {folder.children.map(bookmark => <img className='mr-1 mb-1' height='16' width='16' src={`http://www.google.com/s2/favicons?domain=${bookmark.url}`} />)}
       </div>
     );
   }
