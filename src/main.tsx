7efc90878896307a4f1ca10295a1fdd92dd0914e
Johannes Sörensen <johannes.sorensen@ingka.ikea.com>
2022-05-26 19:31:53 +0200

    Local boostrap and design cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -10,60 +11,51 @@ interface State {
 
 class Bookmarks extends React.Component<any, State> {
 
+  /** Load bookmarks */
   public async componentDidMount() {
-
-    const bookmarks: Bookmark[] = await new Promise((resolve) => chrome.bookmarks.getTree(bookmarks => resolve(bookmarks[0]?.children[0]?.children as any)));
-
-    this.setState({ bookmarks });
+    const bookmarks: Bookmark[] = await new Promise((resolve) => chrome.bookmarks.getTree(resolve));
+    this.setState({ bookmarks: bookmarks[0]?.children[0]?.children });
   }
 
   public render() {
-
     return (
-      <div className='d-flex align-items-center h-100 container'>
-        {this.state?.bookmarks && <div>
+      <div className="h-100" style={{ backgroundColor: '#F0F0F0' }}>
+        <div className='d-flex align-items-center h-75 container'>
+          {this.state?.bookmarks && <div>
 
-          <div className='d-flex align-items-center mb-3' onClick={(event) => this.openFolder(event)}>
-            <h3 className='mb-0'>{this.state?.selectedFolder?.title || 'Favorites'}</h3>
-            {this.state?.selectedFolder && <button type='button' className='btn btn-dark ml-2'>Back</button>}
-          </div>
-
-          {this.renderBookmarksRecursive(this.state?.selectedFolder?.children || this.state.bookmarks)}
-
-        </div>}
-      </div>
-    );
-  }
-
-  private renderBookmarksRecursive(bookmarks: Bookmark[]) {
-    return (
-      <div className='row m-0 flex-wrap'>
-
-        {bookmarks?.map(bookmark => (
-          <a href={bookmark.url} className='text-decoration-none mr-3 mb-4' >
-
-            <div className='d-flex align-items-center border rounded mb-2' style={{ height: 100, width: 100 }} onClick={(event) => this.openFolder(event, bookmark)}>
-              {bookmark.url && <img className='mx-auto' height='24' width='24' src={`http://www.google.com/s2/favicons?domain=${bookmark.url}`} />}
-              {!bookmark.url && this.renderFolderIcons(bookmark)}
+            <div className='d-flex align-items-center mb-3' onClick={(event) => this.openFolder(event)}>
+              <h3 className='mb-0'>{this.state?.selectedFolder?.title || 'Favorites'}</h3>
+              {this.state?.selectedFolder && <button type='button' className='btn btn-dark ml-2'>Back</button>}
             </div>
 
-            <small className='d-block text-dark text-center text-wrap' style={{ width: 100 }}>{bookmark.title}</small>
-          </a>
-        ))}
+            <div className='row m-0 flex-wrap'>
+
+              {(this.state?.selectedFolder?.children || this.state.bookmarks)?.map(bookmark => (
+                <a role="button" href={bookmark.url} className='text-decoration-none mr-3 mb-4' >
+
+                  <div className='d-flex align-items-center border rounded mb-2' style={{ height: 100, width: 100 }} onClick={(event) => this.openFolder(event, bookmark)}>
+                    {bookmark.url && <img className='mx-auto' height='24' width='24' src={`http://www.google.com/s2/favicons?domain=${bookmark.url}`} />}
+                    {!bookmark.url && <div className='d-flex flex-wrap'>
+                      {bookmark.children.map(b => <img className='mr-1 mb-1' height='16' width='16' src={`http://www.google.com/s2/favicons?domain=${b.url}`} />)}
+                    </div>}
+                  </div>
+
+                  <small className='d-block text-dark text-center text-wrap' style={{ width: 100 }}>{bookmark.title}</small>
+                </a>
+              ))}
+            </div>
+
+          </div>}
+        </div>
       </div>
     );
   }
 
-  /** For folders, show contained items in a grid  */
-  private renderFolderIcons(folder: Bookmark) {
-    return (
-      <div className='d-flex flex-wrap'>
-        {folder.children.map(bookmark => <img className='mr-1 mb-1' height='16' width='16' src={`http://www.google.com/s2/favicons?domain=${bookmark.url}`} />)}
-      </div>
-    );
-  }
-
-  /** Choose which folder to view */
+  /**
+   * Choose which folder to view.
+   * @param event
+   * @param bookmark send empty to reset
+   */
   private openFolder(event: React.MouseEvent, bookmark?: Bookmark): void {
     event.preventDefault();
 
@@ -77,4 +69,5 @@ class Bookmarks extends React.Component<any, State> {
 
 }
 
+/** Start app  */
 ReactDOM.render(<Bookmarks />, document.getElementById('app'));
